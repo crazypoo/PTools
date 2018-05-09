c6f7ddf1705044cd091fe7693685db61001591db
mouth <273277355@qq.com>
2018-05-09 08:50:29 +0800

    no message

--- Example/PooTools/PTViewController.m
+++ Example/PooTools/PTViewController.m
@@ -8,9 +8,11 @@
 
 #import "PTViewController.h"
 #import "Utils.h"
+#import "YXCustomAlertView.h"
+#import "PMacros.h"
 
 
-@interface PTViewController ()
+@interface PTViewController ()<YXCustomAlertViewDelegate>
 
 @end
 
@@ -20,6 +22,25 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = self.view.bounds;
+    [btn addTarget:self action:@selector(aaaaa) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+}
+
+-(void)aaaaa
+{
+    YXCustomAlertView *alert = [[YXCustomAlertView alloc] initAlertViewWithFrame:CGRectMake(10, 0, kSCREEN_WIDTH-20, 450) andSuperView:self.view withButtonAndTitleFont:[UIFont systemFontOfSize:20] titleColor:kRandomColor bottomButtonTitleColor:kRandomColor verLineColor:kRandomColor moreButtonTitleArray:@[@"111",@"222",@"123123",@"31231412431241"]];
+    alert.center = CGPointMake(kSCREEN_WIDTH/2, kSCREEN_HEIGHT/2);
+    alert.delegate = self;
+    alert.titleStr = @"111123123123123";
+}
+
+#pragma mark - YXCustomAlertViewDelegate
+-(void)customAlertView:(YXCustomAlertView *)customAlertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    PNSLog(@"%ld",(long)buttonIndex);
 }
 
 - (void)didReceiveMemoryWarning
