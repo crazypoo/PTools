20921f0694718b2ff3977a899e19be62fd20e540
H-L <273277355@qq.com>
2019-01-25 14:05:56 +0800

    save

--- Example/PooTools/PTViewController.m
+++ Example/PooTools/PTViewController.m
@@ -328,7 +328,13 @@ static NSString *cellIdentifier = @"CELL";
                     imageModel3.imageInfo = @"444444";
                     imageModel3.imageTitle = @"333333";
                     
-                    NSArray *arr = @[imageModel2,imageModel3,imageModelT];
+                    PooShowImageModel *imageModel4 = [[PooShowImageModel alloc] init];
+                    imageModel4.imageUrl = kImageNamed(@"DemoImage");
+                    imageModel4.imageShowType = PooShowImageModelTypeNormal;
+                    imageModel4.imageInfo = @"444444";
+                    imageModel4.imageTitle = @"333333";
+                    
+                    NSArray *arr = @[imageModel2,imageModel3,imageModelT,imageModel4];
                     
                     YMShowImageView *ymImageV = [[YMShowImageView alloc] initWithByClick:YMShowImageViewClickTagAppend appendArray:arr titleColor:nil fontName:FontName showImageBackgroundColor:[UIColor blackColor] showWindow:[PTAppDelegate appDelegate].window loadingImageName:@"DemoImage" deleteAble:YES saveAble:YES moreActionImageName:@"DemoImage"];
                     [ymImageV showWithFinish:^{
