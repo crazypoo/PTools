f5fe1ec64db25491c78568a049b4a2087cbdb2ed
邓杰豪 <273277355@qq.com>
2019-03-11 11:01:16 +0800

    增加下载文件方式

---
+++ PooTools/IGFileDownLoadManager.h
@@ -0,0 +1,17 @@
+//
+//  IGFileDownLoadManager.h
+//  CloudGateCustom
+//
+//  Created by 邓杰豪 on 2019/3/11.
+//  Copyright © 2019年 邓杰豪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IGFileDownLoadManager : NSObject
++(void)fileDownloadWithUrl:(NSString * _Nonnull)fileURL withFileSavePath:(NSString * _Nonnull)savePath progress:(void (^)(NSProgress *downloadProgress))downloadProgressBlock completionHandler:(void (^)(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error))block;
+@end
+
+NS_ASSUME_NONNULL_END
