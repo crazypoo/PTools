c85c99b88db2e2aa1bf269f83484741f28efbfde
傑豪 鄧 <273277355@qq.com>
2019-08-29 17:53:01 +0800

    bug fix

--- PooTools/NetWork/PTUploadDataSteamTools.m
+++ PooTools/NetWork/PTUploadDataSteamTools.m
@@ -21,69 +21,71 @@
 
 +(void)uploadComboDataSteamProgressInView:(UIView *)view withParameters:(NSDictionary *)parameters withServerAddress:(NSString *)serverAddress imageArray:(NSArray <PTUploadDataModel *>*)dataModelArr timeOut:(NSTimeInterval)timeoutInterval success:(PTUploadDataToServerSuccessBlock)successBlock failure:(PTUploadDataToServerFailureBlock)failureBlock
 {
-    HZWaitingView *orView = [[HZWaitingView alloc] init];
-    __weak HZWaitingView *waitingView = orView;
-    dispatch_async(dispatch_get_main_queue(), ^{
-        if (view)
-        {
-            waitingView.mode = HZWaitingViewModeLoopDiagram;
-            [view addSubview:waitingView];
-            [waitingView mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.width.height.offset(kSCREEN_WIDTH * 0.5);
-                make.centerX.centerY.equalTo(view);
-            }];
-        }
-    });
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            HZWaitingView *orView = [[HZWaitingView alloc] init];
+            __strong HZWaitingView *waitingView = orView;
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (view)
+                {
+                    waitingView.mode = HZWaitingViewModeLoopDiagram;
+                    [view addSubview:waitingView];
+                    [waitingView mas_makeConstraints:^(MASConstraintMaker *make) {
+                        make.width.height.offset(kSCREEN_WIDTH * 0.5);
+                        make.centerX.centerY.equalTo(view);
+                    }];
+                }
+            });
 
-    kShowNetworkActivityIndicator();
-    
-    //表单请求，上传文件
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    manager.requestSerializer = [AFJSONRequestSerializer serializer];//请求
-    manager.responseSerializer = [AFHTTPResponseSerializer serializer];//响应
-    manager.requestSerializer.timeoutInterval = timeoutInterval;
-    [manager.responseSerializer setAcceptableContentTypes:[NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"textml",@"text/css",@"text/plain", @"application/javascript",@"application/json", @"application/x-www-form-urlencoded",@"multipart/form-data",@"image/jpg",@"image/png",@"image/jpeg",@"video/mp4",@"audio/mpeg",@"flv/qsv",@"mp3/mp3",@"application/octet-stream",@"text/html",@"groupFile/zip", nil]];
-    
-#if DEBUG
-    PNSLog(@"Address:%@\nParameters:%@",serverAddress,parameters);
-#endif
-    [manager POST:serverAddress parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData> _Nonnull formData){
-        [WMHub hide];
-        for (int i = 0; i < dataModelArr.count; i++)
-        {
-            PTUploadDataModel *model = dataModelArr[i];
-            [formData appendPartWithFileData:model.imageData name:model.imageDataName fileName:model.imageName mimeType:[PTUploadDataSteamTools imageMimeTypeWith:model.imageType]];
-        }
-    }progress:^(NSProgress *uploadProgress){
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (view)
-            {
-                waitingView.progress = uploadProgress.fractionCompleted;
-            }
-        });
-    }success:^(NSURLSessionDataTask *task, id responseObject){
-        if (view)
-        {
-            [waitingView removeFromSuperview];
-        }
-        kHideNetworkActivityIndicator();
-        NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers error:nil];
-#if DEBUG
-        PNSLog(@"ServerSuccessReturn:%@",dic);
-#endif
-        if (successBlock) {
-            successBlock(dic);
-        }
-    }failure:^(NSURLSessionDataTask *task, NSError *error){
-        [waitingView removeFromSuperview];
-        kHideNetworkActivityIndicator();
-#if DEBUG
-        PNSLog(@"ServerFailureReturn:%@",error);
-#endif
-        if (failureBlock) {
-            failureBlock(error);
-        }
-    }];
+            kShowNetworkActivityIndicator();
+            
+            //表单请求，上传文件
+            AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+            manager.requestSerializer = [AFJSONRequestSerializer serializer];//请求
+            manager.responseSerializer = [AFHTTPResponseSerializer serializer];//响应
+            manager.requestSerializer.timeoutInterval = timeoutInterval;
+            [manager.responseSerializer setAcceptableContentTypes:[NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"textml",@"text/css",@"text/plain", @"application/javascript",@"application/json", @"application/x-www-form-urlencoded",@"multipart/form-data",@"image/jpg",@"image/png",@"image/jpeg",@"video/mp4",@"audio/mpeg",@"flv/qsv",@"mp3/mp3",@"application/octet-stream",@"text/html",@"groupFile/zip", nil]];
+            
+        #if DEBUG
+            PNSLog(@"Address:%@\nParameters:%@",serverAddress,parameters);
+        #endif
+            [manager POST:serverAddress parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData> _Nonnull formData){
+                [WMHub hide];
+                for (int i = 0; i < dataModelArr.count; i++)
+                {
+                    PTUploadDataModel *model = dataModelArr[i];
+                    [formData appendPartWithFileData:model.imageData name:model.imageDataName fileName:model.imageName mimeType:[PTUploadDataSteamTools imageMimeTypeWith:model.imageType]];
+                }
+            }progress:^(NSProgress *uploadProgress){
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (view)
+                    {
+                        waitingView.progress = uploadProgress.fractionCompleted;
+                    }
+                });
+            }success:^(NSURLSessionDataTask *task, id responseObject){
+                if (view)
+                {
+                    [waitingView removeFromSuperview];
+                }
+                kHideNetworkActivityIndicator();
+                NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers error:nil];
+        #if DEBUG
+                PNSLog(@"ServerSuccessReturn:%@",dic);
+        #endif
+                if (successBlock) {
+                    successBlock(dic);
+                }
+            }failure:^(NSURLSessionDataTask *task, NSError *error){
+                [waitingView removeFromSuperview];
+                kHideNetworkActivityIndicator();
+        #if DEBUG
+                PNSLog(@"ServerFailureReturn:%@",error);
+        #endif
+                if (failureBlock) {
+                    failureBlock(error);
+                }
+            }];
+    });
 }
 
 +(NSString *)imageMimeTypeWith:(CGUploadType)type
