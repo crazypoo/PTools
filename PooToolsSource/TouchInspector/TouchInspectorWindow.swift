7be2ceb28e27edaf20bb3e6b6823bb5b1b8f396b
Jax <273277355@qq.com>
2022-09-13 10:27:52 +0800

    bug fix

--- PooToolsSource/TouchInspector/TouchInspectorWindow.swift
+++ PooToolsSource/TouchInspector/TouchInspectorWindow.swift
@@ -48,6 +48,10 @@ public class TouchInspectorWindow: UIWindow {
         super.init(windowScene: windowScene)
     }
     
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
