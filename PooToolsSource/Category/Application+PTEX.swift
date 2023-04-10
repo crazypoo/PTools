caf13017e8c2d6aa375e14d0f3c9c9e4ea29c686
jax <273277355@qq.com>
2023-04-10 22:47:07 +0800

    優化辣雞代碼

--- PooToolsSource/Category/Application+PTEX.swift
+++ PooToolsSource/Category/Application+PTEX.swift
@@ -10,12 +10,10 @@ import UIKit
 
 extension UIApplication: PTProtocolCompatible { }
 
-public extension PTProtocol where Base: UIApplication
-{
+public extension PTProtocol where Base: UIApplication {
     //MARK: 获取应用的location
     ///获取应用的location
-    static var currentApplicationLocal:String
-    {
+    static var currentApplicationLocal:String {
         let locale:NSLocale = NSLocale.current as NSLocale
         let countryCode = locale.object(forKey: NSLocale.Key.countryCode)
         let usLocale = NSLocale.init(localeIdentifier: "en_US")
@@ -28,35 +26,26 @@ public extension PTProtocol where Base: UIApplication
      */
     //MARK: iOS更换App图标
     ///iOS更换App图标
-    static func changeAppIcon()
-    {
-        if UIApplication.shared.supportsAlternateIcons
-        {
+    static func changeAppIcon() {
+        if UIApplication.shared.supportsAlternateIcons {
             PTNSLogConsole("you can change this app's icon")
-        }
-        else
-        {
+        } else {
             PTNSLogConsole("you can not change this app's icon")
             return
         }
         
         let iconName = UIApplication.shared.alternateIconName
-        if !(iconName ?? "").stringIsEmpty()
-        {
+        if !(iconName ?? "").stringIsEmpty() {
             UIApplication.shared.setAlternateIconName(nil) { error in
-                if error != nil
-                {
+                if error != nil {
                     UIViewController.gobal_drop(title: error.debugDescription)
 
                 }
                 PTNSLogConsole("The alternate icon's name is \(String(describing: iconName))")
             }
-        }
-        else
-        {
+        } else {
             UIApplication.shared.setAlternateIconName(iconName) { error in
-                if error != nil
-                {
+                if error != nil {
                     UIViewController.gobal_drop(title: error.debugDescription)
 
                 }
@@ -67,8 +56,7 @@ public extension PTProtocol where Base: UIApplication
     
     //MARK: 类似iPhone点击了Home键
     ///类似iPhone点击了Home键
-    static func likeTapHome()
-    {
+    static func likeTapHome() {
         PTGCDManager.gcdMain {
             UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
         }
