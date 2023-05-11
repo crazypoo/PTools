66b20c8790cb9a94fc7b600f12e4eb1eba0cd78c
jax <273277355@qq.com>
2023-05-12 07:25:52 +0800

    添加一些辣鸡功能

--- PooToolsSource/Core/PTUtils.swift
+++ PooToolsSource/Core/PTUtils.swift
@@ -143,6 +143,32 @@ public class PTUtils: NSObject {
         }
     }
     
+    @objc open class dynamic func topMost(of viewController: UIViewController?) -> UIViewController? {
+        // presented view controller
+        if let presentedViewController = viewController?.presentedViewController {
+            return topMost(of: presentedViewController)
+        }
+
+        // UITabBarController
+        if let tabBarController = viewController as? UITabBarController,
+            let selectedViewController = tabBarController.selectedViewController {
+            return topMost(of: selectedViewController)
+        }
+        
+        // UINavigationController
+        if let navigationController = viewController as? UINavigationController,
+            let visibleViewController = navigationController.visibleViewController {
+            return topMost(of: visibleViewController)
+        }
+        
+        // UIPageController
+        if let pageViewController = viewController as? UIPageViewController,
+           pageViewController.viewControllers?.count == 1 {
+            return topMost(of: pageViewController.viewControllers?.first)
+        }
+        return viewController
+    }
+    
     public class func cgBaseBundle()->Bundle {
         let bundle = Bundle.init(for: self)
         return bundle
