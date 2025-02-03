a5f4ada6b77f6ad04e2bf08a6826dd1f11cc6c44
jax <273277355@qq.com>
2025-02-03 20:21:48 +0800

    添加一些扩展

--- PooToolsSource/Core/PTUtils.swift
+++ PooToolsSource/Core/PTUtils.swift
@@ -198,6 +198,20 @@ public class PTUtils: NSObject {
             return URL(fileURLWithPath: outputURL)
         }
     }
+    
+    /// 字符串转类
+    public class func classFromString(_ className:String) -> AnyClass? {
+        // 1、获swift中的命名空间名
+        var name = Bundle.main.object(forInfoDictionaryKey: "CFBundleExecutable") as? String
+        // 2、如果包名中有'-'横线这样的字符，在拿到包名后，还需要把包名的'-'转换成'_'下横线
+        name = name?.replacingOccurrences(of: "-", with: "_")
+        // 3、拼接命名空间和类名，”包名.类名“
+        let fullClassName = name! + "." + className
+        // 通过NSClassFromString获取到最终的类
+        let anyClass: AnyClass? = NSClassFromString(fullClassName)
+        // 本类type
+        return anyClass
+    }
 }
 
 //MARK: 寻找界面
