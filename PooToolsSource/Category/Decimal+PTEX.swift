a88b87de0905a1c4feb7bb71f11c66867fa158ac
FrokingMonkey <273277355@qq.com>
2025-05-07 09:52:37 +0800

    修復一些問題

--- PooToolsSource/Category/Decimal+PTEX.swift
+++ PooToolsSource/Category/Decimal+PTEX.swift
@@ -15,4 +15,20 @@ public extension Decimal {
         NSDecimalRound(&roundedTotalDiscountAmount, &newPrice, 2, .bankers)
         return roundedTotalDiscountAmount
     }
+    
+    static func bankPriceFor3(price:Decimal) -> Decimal {
+        let handler = NSDecimalNumberHandler(
+            roundingMode: .up,           // 無條件進位
+            scale: 2,                    // 保留兩位小數
+            raiseOnExactness: false,
+            raiseOnOverflow: false,
+            raiseOnUnderflow: false,
+            raiseOnDivideByZero: false
+        )
+
+        let decimalNumber = NSDecimalNumber(decimal: price)
+        let rounded = decimalNumber.rounding(accordingToBehavior: handler)
+
+        return rounded.decimalValue
+    }
 }
