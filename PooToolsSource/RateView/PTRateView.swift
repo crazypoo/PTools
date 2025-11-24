a8024031ac63fcba7838c4e1e9f77caa7385d5b7
jax <273277355@qq.com>
2025-11-24 15:10:51 +0800

    代碼優化

--- PooToolsSource/RateView/PTRateView.swift
+++ PooToolsSource/RateView/PTRateView.swift
@@ -41,123 +41,149 @@ public class PTRateConfig: NSObject {
     public var imageContentMode:UIView.ContentMode = .scaleAspectFill
 }
 
+// MARK: - View
 @objcMembers
 public class PTRateView: UIView {
-    public var rateBlock:PTRateScoreBlock?
-    
-    public var viewConfig:PTRateConfig? {
+
+    // MARK: Public
+    public var rateBlock: PTRateScoreBlock?
+    public var viewConfig: PTRateConfig? {
         didSet {
             guard let config = viewConfig else { return }
             scorePercent = config.scorePercent
-            removeSubviews()
-            loaded = false
-            initView()
+            reloadUI()
         }
     }
-    fileprivate lazy var backgroundStarView:UIView = createStartView(image: viewConfig!.bImage, tag: PTRateBackgroundViewTags)
-    fileprivate lazy var foregroundStarView:UIView = createStartView(image: viewConfig!.fImage, tag: PTRateForegroundViewTags)
 
-    fileprivate var scorePercent:CGFloat? = 0 {
-        didSet {
-            guard let scorePercent = scorePercent else { return }
-            PTGCDManager.gcdAfter(time: 0.1) {
-                self.layoutSubviews()
-            }
-            
-            rateBlock?(scorePercent)
-        }
-    }
+    // MARK: Private
+    private var scorePercent: CGFloat = 1
+    private var isPrepared = false
     
-    fileprivate var loaded:Bool = false
-    
-    public init(viewConfig:PTRateConfig) {
-        super.init(frame: .zero)
+    private let backgroundStarView = UIView()
+    private let foregroundStarView = UIView()
+
+    // MARK: - Init
+    public init(viewConfig: PTRateConfig) {
         self.viewConfig = viewConfig
-        initView()
-    }
-    
-    func initView() {
-        guard let config = viewConfig else { return }
-        if config.canTap {
-            let tapGes = UITapGestureRecognizer { [weak self] sender in
-                guard let self = self else { return }
-                if let ges = sender as? UITapGestureRecognizer {
-                    let tapPoint = ges.location(in: self)
-                    self.handleTapGesture(tapPoint)
-                }
-            }
-            tapGes.numberOfTapsRequired = 1
-            addGestureRecognizers([tapGes])
-        }
-        
-        scorePercent = config.scorePercent
-
-        PTGCDManager.gcdAfter(time: 0.1) {
-            self.addSubviews([self.backgroundStarView,self.foregroundStarView])
-
-            self.backgroundStarView.snp.makeConstraints({ make in
-                make.edges.equalToSuperview()
-            })
-            
-            self.foregroundStarView.snp.makeConstraints({ make in
-                make.top.bottom.equalToSuperview()
-                make.left.equalToSuperview()
-                make.width.equalTo(0)
-            })
-            
-            self.updateForegroundStarViewWidth(animated: config.hadAnimation)
-            self.loaded = true
-        }
-    }
-    
-    public override func layoutSubviews() {
-        super.layoutSubviews()
-    }
-    
-    fileprivate func handleTapGesture(_ tapPoint: CGPoint) {
-        guard let config = viewConfig else { return }
-        let offSet = tapPoint.x
-        let realStarScore = offSet / (self.frame.size.width / CGFloat(config.numberOfStar))
-        let starScore = config.allowIncompleteStar ? Float(realStarScore) : ceilf(Float(realStarScore))
-        self.scorePercent = CGFloat(starScore) / CGFloat(config.numberOfStar)
-        self.updateForegroundStarViewWidth(animated: config.hadAnimation)
-    }
-        
-    fileprivate func updateForegroundStarViewWidth(animated: Bool = false) {
-        guard let scorePercent = scorePercent else { return }
-        let foregroundWidth = self.frame.size.width * scorePercent
-        let animationTimeInterval = animated ? 0.2 : 0
-        UIView.animate(withDuration: animationTimeInterval) {
-            self.foregroundStarView.snp.updateConstraints { make in
-                make.width.equalTo(foregroundWidth)
-            }
-        }
+        super.init(frame: .zero)
+        setupBaseUI()
+        reloadUI()
     }
 
-    fileprivate func createStartView(image: UIImage, tag: Int) -> UIView {
-        guard let config = viewConfig else { return UIView() }
-        let contentV = UIView()
-        contentV.clipsToBounds = true
-        contentV.backgroundColor = .clear
-        contentV.isUserInteractionEnabled = tag == PTRateForegroundViewTags ? true : false
-
-        let itemW = (self.frame.size.width - config.itemSpacing * CGFloat(config.numberOfStar - 1)) / CGFloat(config.numberOfStar)
-        
-        for i in 0..<config.numberOfStar {
-            let imageV = UIImageView(image: image)
-            imageV.contentMode = config.imageContentMode
-            imageV.tag = tag + i
-            contentV.addSubview(imageV)
-            imageV.snp.makeConstraints { make in
-                make.left.equalToSuperview().inset(CGFloat(i) * itemW + config.itemSpacing * CGFloat(i))
-                make.top.bottom.equalToSuperview()
-                make.width.equalTo(itemW)
-            }
-        }
-        return contentV
-    }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Setup
+    private func setupBaseUI() {
+        backgroundColor = .clear
+        
+        addSubviews([backgroundStarView, foregroundStarView])
+        backgroundStarView.clipsToBounds = true
+        foregroundStarView.clipsToBounds = true
+
+        backgroundStarView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        foregroundStarView.snp.makeConstraints { make in
+            make.left.top.bottom.equalToSuperview()
+            make.width.equalTo(0)
+        }
+    }
+
+    private func reloadUI() {
+        guard let config = viewConfig else { return }
+
+        backgroundStarView.removeSubviews()
+        foregroundStarView.removeSubviews()
+
+        createStars(in: backgroundStarView,
+                    image: config.bImage,
+                    baseTag: PTRateBackgroundViewTags)
+
+        createStars(in: foregroundStarView,
+                    image: config.fImage,
+                    baseTag: PTRateForegroundViewTags)
+
+        if config.canTap {
+            addTapGesture()
+        }
+
+        setNeedsLayout()
+        layoutIfNeeded()
+        updateForegroundWidth(animated: config.hadAnimation)
+    }
+
+    // MARK: Layout
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        updateStarFrames()
+        updateForegroundWidth(animated: false)
+    }
+
+    private func updateStarFrames() {
+        guard let config = viewConfig else { return }
+        
+        let count = config.numberOfStar
+        let totalSpacing = config.itemSpacing * CGFloat(count - 1)
+        let itemW = (bounds.width - totalSpacing) / CGFloat(count)
+
+        for i in 0..<count {
+            let left = CGFloat(i) * (itemW + config.itemSpacing)
+            
+            if let bg = backgroundStarView.viewWithTag(PTRateBackgroundViewTags + i) {
+                bg.frame = CGRect(x: left, y: 0, width: itemW, height: bounds.height)
+            }
+            if let fg = foregroundStarView.viewWithTag(PTRateForegroundViewTags + i) {
+                fg.frame = CGRect(x: left, y: 0, width: itemW, height: bounds.height)
+            }
+        }
+    }
+
+    private func updateForegroundWidth(animated: Bool) {
+        let width = bounds.width * scorePercent
+        foregroundStarView.snp.updateConstraints { make in
+            make.width.equalTo(width)
+        }
+        
+        UIView.animate(withDuration: animated ? 0.2 : 0.0) {
+            self.layoutIfNeeded()
+        }
+    }
+
+    // MARK: Stars Builder
+    private func createStars(in container: UIView, image: UIImage, baseTag: Int) {
+        guard let config = viewConfig else { return }
+
+        for i in 0..<config.numberOfStar {
+            let imgV = UIImageView(image: image)
+            imgV.contentMode = config.imageContentMode
+            imgV.tag = baseTag + i
+            container.addSubview(imgV)
+        }
+    }
+
+    // MARK: Tap Logic
+    private func addTapGesture() {
+        gestureRecognizers?.forEach { removeGestureRecognizer($0) }
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        addGestureRecognizer(tap)
+    }
+
+    @objc private func handleTap(_ tap: UITapGestureRecognizer) {
+        guard let config = viewConfig else { return }
+
+        let x = tap.location(in: self).x
+        let starWidth = bounds.width / CGFloat(config.numberOfStar)
+        
+        let raw = x / starWidth
+        let score = config.allowIncompleteStar ? CGFloat(raw) : CGFloat(ceil(raw))
+        
+        scorePercent = min(max(score / CGFloat(config.numberOfStar), 0), 1)
+
+        updateForegroundWidth(animated: config.hadAnimation)
+        rateBlock?(scorePercent)
     }
 }
