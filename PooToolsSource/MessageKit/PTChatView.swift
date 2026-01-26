809bea5fbb9bf7edb5f722a98c7f8a76e0d1bf11
jax <273277355@qq.com>
2026-01-26 17:58:05 +0800

    bug fix

--- PooToolsSource/MessageKit/PTChatView.swift
+++ PooToolsSource/MessageKit/PTChatView.swift
@@ -69,85 +69,84 @@ public class PTChatView: UIView {
         view.registerClassCells(classs: [PTChatSystemMessageCell.ID:PTChatSystemMessageCell.self,PTChatTextCell.ID:PTChatTextCell.self,PTChatMediaCell.ID:PTChatMediaCell.self,PTChatMapCell.ID:PTChatMapCell.self,PTChatVoiceCell.ID:PTChatVoiceCell.self,PTChatTypingIndicatorCell.ID:PTChatTypingIndicatorCell.self,PTChatFileCell.ID:PTChatFileCell.self])
         view.customerLayout = { sectionIndex,sectionModel in
             return UICollectionView.waterFallLayout(data: sectionModel.rows,rowCount:1,itemOriginalX: 0,topContentSpace:PTChatConfig.share.chatTopFixel, bottomContentSpace: PTChatConfig.share.chatBottomFixel,itemSpace: 0) { index, model in
-                if let row = model as? PTRows,let cellModel = row.dataModel as? PTChatListModel {
-                    var cellHeight:CGFloat = 0
-                    
-                    let timeHeight = (PTChatConfig.share.showTimeLabel ? (PTChatConfig.share.chatTimeFont.pointSize + 15) : 0)
-                    let nameHeight = (PTChatConfig.share.showSenderName ? (PTChatConfig.share.senderNameFont.pointSize + 10) : 0)
-                    let readStatusHeight = PTChatConfig.share.showReadStatus ? (PTChatConfig.share.readStatusFont.pointSize + 10) : 0
-                    let spaceHeight = PTChatBaseCell.timeTopSpace * 2
-                    
-                    switch cellModel.messageType {
-                    case .Text:
-                        if let msgContent = cellModel.msgContent as? String {
-                            
-                            var dataContentFont:UIFont!
-                            if cellModel.belongToMe {
-                                dataContentFont = PTChatConfig.share.textMeMessageFont
-                            } else {
-                                dataContentFont = PTChatConfig.share.textOtherMessageFont
-                            }
-                            
-                            var contentHeight = UIView.sizeFor(string: msgContent, font: dataContentFont,lineSpacing: PTChatConfig.share.textLineSpace,width: PTChatConfig.ChatContentShowMaxWidth).height + 40
-                            
-                            let contentNumberOfLines = msgContent.numberOfLines(font: dataContentFont, labelShowWidth: PTChatConfig.ChatContentShowMaxWidth,lineSpacing: PTChatConfig.share.textLineSpace)
-                            if contentNumberOfLines <= 1 {
-                                contentHeight = PTChatConfig.share.contentBaseHeight
-                            }
+                let cellModel = self.chatDataArr[index]
+                var cellHeight:CGFloat = 0
+                
+                let timeHeight = (PTChatConfig.share.showTimeLabel ? (PTChatConfig.share.chatTimeFont.pointSize + 15) : 0)
+                let nameHeight = (PTChatConfig.share.showSenderName ? (PTChatConfig.share.senderNameFont.pointSize + 10) : 0)
+                let readStatusHeight = PTChatConfig.share.showReadStatus ? (PTChatConfig.share.readStatusFont.pointSize + 10) : 0
+                let spaceHeight = PTChatBaseCell.timeTopSpace * 2
+                
+                switch cellModel.messageType {
+                case .Text:
+                    if let msgContent = cellModel.msgContent as? String {
                         
-                            var nameContentTotal = nameHeight + contentHeight + readStatusHeight
-                            if nameContentTotal < PTChatConfig.share.messageUserIconSize {
-                                nameContentTotal = PTChatConfig.share.messageUserIconSize
-                            }
-                            
-                            cellHeight = nameContentTotal + timeHeight + spaceHeight
-                        }
-                    case .Map:
-                        let mapHeight = PTChatConfig.share.mapMessageImageHeight
-                        cellHeight = timeHeight + nameHeight + mapHeight + spaceHeight + readStatusHeight
-                    case .Media:
-                        let imageHeight = PTChatConfig.share.imageMessageImageHeight
-                        cellHeight = timeHeight + nameHeight + imageHeight + spaceHeight + readStatusHeight
-                    case .Voice:
-                        let voiceHeight:CGFloat = 38
-                        cellHeight = timeHeight + nameHeight + voiceHeight + spaceHeight + readStatusHeight
-                    case .File:
-                        var url:URL?
-                        if let contentString = cellModel.msgContent as? String {
-                            url = URL(string: contentString)
-                        } else if let contentURL = cellModel.msgContent as? URL {
-                            url = contentURL
+                        var dataContentFont:UIFont!
+                        if cellModel.belongToMe {
+                            dataContentFont = PTChatConfig.share.textMeMessageFont
+                        } else {
+                            dataContentFont = PTChatConfig.share.textOtherMessageFont
                         }
                         
-                        var contentHeight:CGFloat = PTChatFileCell.FileCellHeight
-                        if let findURL = url {
-                            let nameHeight = UIView.sizeFor(string: findURL.lastPathComponent, font: PTChatConfig.share.fileNameFont,lineSpacing: PTChatConfig.share.fileContentSpace,width: PTChatFileCell.FileConentWidth - PTChatFileCell.FileCellImageHeight - PTChatFileCell.FileCellConentFixbel * 3).height
-                            let fileSizeHeight = PTChatConfig.share.fileSizeFont.pointSize + 2 + PTChatFileCell.FileCellConentFixbel * 2
-                            let total = nameHeight + fileSizeHeight
-                            if total >= contentHeight {
-                                contentHeight = total
-                            }
+                        var contentHeight = UIView.sizeFor(string: msgContent, font: dataContentFont,lineSpacing: PTChatConfig.share.textLineSpace,width: PTChatConfig.ChatContentShowMaxWidth).height + 40
+                        
+                        let contentNumberOfLines = msgContent.numberOfLines(font: dataContentFont, labelShowWidth: PTChatConfig.ChatContentShowMaxWidth,lineSpacing: PTChatConfig.share.textLineSpace)
+                        if contentNumberOfLines <= 1 {
+                            contentHeight = PTChatConfig.share.contentBaseHeight
                         }
-                        cellHeight = timeHeight + contentHeight + nameHeight + PTChatBaseCell.timeTopSpace * 3 + readStatusHeight
-                    case .SystemMessage:
-                        let timeHeight = UIView.sizeFor(string: cellModel.messageTimeStamp.timeToDate().toFormat("yyyy-MM-dd HH:MM:ss"), font: PTChatConfig.share.chatTimeFont,lineSpacing: 2,width: CGFloat.kSCREEN_WIDTH).height
-                        var contentHeight:CGFloat = 0
-                        if let contentString = cellModel.msgContent as? String {
-                            contentHeight = UIView.sizeFor(string: contentString, font: PTChatConfig.share.chatSystemMessageFont,lineSpacing: 2,width: CGFloat.kSCREEN_WIDTH).height
+                    
+                        var nameContentTotal = nameHeight + contentHeight + readStatusHeight
+                        if nameContentTotal < PTChatConfig.share.messageUserIconSize {
+                            nameContentTotal = PTChatConfig.share.messageUserIconSize
                         }
-                        cellHeight = timeHeight + contentHeight + 20
-                    case .Typing:
-                        cellHeight = 44
-                    case .CustomerMessage:
-                        cellHeight = self.customerCellHeightHandler?(cellModel,index) ?? 44
+                        
+                        cellHeight = nameContentTotal + timeHeight + spaceHeight
                     }
-                    return cellHeight
+                case .Map:
+                    let mapHeight = PTChatConfig.share.mapMessageImageHeight
+                    cellHeight = timeHeight + nameHeight + mapHeight + spaceHeight + readStatusHeight
+                case .Media:
+                    let imageHeight = PTChatConfig.share.imageMessageImageHeight
+                    cellHeight = timeHeight + nameHeight + imageHeight + spaceHeight + readStatusHeight
+                case .Voice:
+                    let voiceHeight:CGFloat = 38
+                    cellHeight = timeHeight + nameHeight + voiceHeight + spaceHeight + readStatusHeight
+                case .File:
+                    var url:URL?
+                    if let contentString = cellModel.msgContent as? String {
+                        url = URL(string: contentString)
+                    } else if let contentURL = cellModel.msgContent as? URL {
+                        url = contentURL
+                    }
+                    
+                    var contentHeight:CGFloat = PTChatFileCell.FileCellHeight
+                    if let findURL = url {
+                        let nameHeight = UIView.sizeFor(string: findURL.lastPathComponent, font: PTChatConfig.share.fileNameFont,lineSpacing: PTChatConfig.share.fileContentSpace,width: PTChatFileCell.FileConentWidth - PTChatFileCell.FileCellImageHeight - PTChatFileCell.FileCellConentFixbel * 3).height
+                        let fileSizeHeight = PTChatConfig.share.fileSizeFont.pointSize + 2 + PTChatFileCell.FileCellConentFixbel * 2
+                        let total = nameHeight + fileSizeHeight
+                        if total >= contentHeight {
+                            contentHeight = total
+                        }
+                    }
+                    cellHeight = timeHeight + contentHeight + nameHeight + PTChatBaseCell.timeTopSpace * 3 + readStatusHeight
+                case .SystemMessage:
+                    let timeHeight = UIView.sizeFor(string: cellModel.messageTimeStamp.timeToDate().toFormat("yyyy-MM-dd HH:MM:ss"), font: PTChatConfig.share.chatTimeFont,lineSpacing: 2,width: CGFloat.kSCREEN_WIDTH).height
+                    var contentHeight:CGFloat = 0
+                    if let contentString = cellModel.msgContent as? String {
+                        contentHeight = UIView.sizeFor(string: contentString, font: PTChatConfig.share.chatSystemMessageFont,lineSpacing: 2,width: CGFloat.kSCREEN_WIDTH).height
+                    }
+                    cellHeight = timeHeight + contentHeight + 20
+                case .Typing:
+                    cellHeight = 44
+                case .CustomerMessage:
+                    cellHeight = self.customerCellHeightHandler?(cellModel,index) ?? 44
                 }
-                return 0
+                return cellHeight
             }
         }
         view.cellInCollection = { collectionView,sectionModel,indexPath in
-            if let itemRow = sectionModel.rows?[indexPath.row],let cellModel = itemRow.dataModel as? PTChatListModel {
+            if let itemRow = sectionModel.rows?[indexPath.row] {
+                let cellModel = self.chatDataArr[indexPath.row]
                 let baseCell = collectionView.dequeueReusableCell(withReuseIdentifier: itemRow.ID, for: indexPath)
                 if itemRow.ID == PTChatSystemMessageCell.ID,let cell = baseCell as? PTChatSystemMessageCell {
                     cell.cellModel = cellModel
