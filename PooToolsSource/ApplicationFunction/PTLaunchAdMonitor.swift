11799d1b0716b179eb3f094ee893a5ad765a95fa
jax <273277355@qq.com>
2026-05-05 12:19:39 +0800

    ex

--- PooToolsSource/ApplicationFunction/PTLaunchAdMonitor.swift
+++ PooToolsSource/ApplicationFunction/PTLaunchAdMonitor.swift
@@ -181,10 +181,9 @@ public class PTLaunchAdMonitor: NSObject {
             make.top.equalToSuperview().inset(CGFloat.statusBarHeight())
         }
         
-        PTGCDManager.gcdMain {
-            self.skipButton.viewCorner(radius: 22)
-            self.playerLayer.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - self.bottomViewHeight)
-        }
+        self.skipButton.layoutIfNeeded()
+        self.skipButton.viewCorner(radius: 22)
+        self.playerLayer.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - self.bottomViewHeight)
     }
     
     // MARK: - 私有方法：启动广告序列
