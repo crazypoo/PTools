f8132b5d63b9187354281eb47108419f74489bf1
jax <1111976+crazypoo@users.noreply.github.com>
2026-07-09 15:04:40 +0800

    ex

--- PooToolsSource/ImageEditor/PTEditImageToolEngine.swift
+++ PooToolsSource/ImageEditor/PTEditImageToolEngine.swift
@@ -129,15 +129,15 @@ public class PTPassthroughView: UIView {
 // 所有交互式编辑工具（涂鸦、马赛克等）的通用协议
 @MainActor public protocol PTEditImageToolEngine: AnyObject {
     /// 关联的画布视图（引擎在这个视图上进行渲染）
-    public var canvasView: UIView { get }
+    var canvasView: UIView { get }
     /// 工具被选中时触发
-    public func toolDidActivate()
+    func toolDidActivate()
     /// 工具被取消选中时触发
-    public func toolDidDeactivate()
+    func toolDidDeactivate()
     /// 处理拖拽手势
-    public func handlePanGesture(_ pan: UIPanGestureRecognizer)
+    func handlePanGesture(_ pan: UIPanGestureRecognizer)
     /// 撤销/重做操作时，通知引擎刷新图层
-    public func reloadRenderState()
+    func reloadRenderState()
 }
 
 public class PTDrawEngine: NSObject, PTEditImageToolEngine {
